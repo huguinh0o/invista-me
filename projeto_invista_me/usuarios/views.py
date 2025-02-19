da9502b1f2e5dc5011d55b08db52c07f270584a2
huguinh0o <hugomurilopb@gmail.com>
2025-02-19 08:01:13 -0300

    cadastro usuario

--- projeto_invista_me/usuarios/views.py
+++ projeto_invista_me/usuarios/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def novo_usuario(request):
-    formulario = UserCreationForm()
+    #tipo, validar, informar e salvar
+    if request.method == 'POST':
+        formulario = UserCreationForm(request.POST)
+        if formulario.is_valid():
+            #salvar
+            formulario.save()
+            #informar
+            usuario = formulario.cleaned_data.get('username')
+            messages.success(request, f'O usuario {usuario} foi criado com sucesso!')
+            return redirect('investimentos')           
     return render(request, 'usuarios/registrar.html', {'formulario': formulario})
 
